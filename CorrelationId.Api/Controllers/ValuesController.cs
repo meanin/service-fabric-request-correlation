56c6e7f29f3c204a1ac8ad494ca10b23d0b31f3e
Paweł Ruciński <Pawel.Rucinski@gyldendal.no>
2019-01-11 16:07:21 +0100

    Initialized solution

---
+++ CorrelationId.Api/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CorrelationId.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello world!");
+        }
+    }
+}
